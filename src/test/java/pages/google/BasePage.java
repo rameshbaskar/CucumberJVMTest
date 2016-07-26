b61700ae5c818734023125f5f746fe0d6e9cf7ff
Ramesh Baskarasubramanian <ramesh.baskarasubramanian@live.com>
2016-07-26 16:06:33 +0530

    Added the ability for an on-demand element finder

--- src/test/java/pages/google/BasePage.java
+++ src/test/java/pages/google/BasePage.java
@@ -1,5 +1,6 @@
 package pages.google;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,5 +18,33 @@ class BasePage {
     WebElement elementWithWaitFor(WebElement webElement) {
         WebDriverWait wait = new WebDriverWait(driver(), 30);
         return wait.until(ExpectedConditions.visibilityOf(webElement));
+    }
+
+    WebElement elementWithWaitFor(String locator) {
+        String strategy = locator.split("=")[0];
+        String value = locator.split("=")[1];
+        return elementWithWaitFor(driver().findElement(getFinder(strategy, value)));
+    }
+
+    private By getFinder(String strategy, String value) {
+        By finder = null;
+
+        if (strategy.equals("name")) {
+            finder = By.name(value);
+        }
+
+        if (strategy.equals("id")) {
+            finder = By.id(value);
+        }
+
+        if (strategy.equals("css")) {
+            finder = By.cssSelector(value);
+        }
+
+        if (strategy.equals("xpath")) {
+            finder = By.xpath(value);
+        }
+
+        return finder;
     }
 }
